f0793074e5df235293e9b0342411f87bc4bd25c0
KyleWoo2619 <wkhgate2619@gmail.com>
2024-10-14 20:23:48 -0400

    Fixed Kyle Interaction

--- Assets/PuzzleManager.cs
+++ Assets/PuzzleManager.cs
@@ -2,58 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.UI;
-using UnityEngine.Events;
+using TMPro; // For TextMeshPro
 
 public class PuzzleManager : MonoBehaviour
 {
-    public XRSocketInteractor socket1; // Socket for the first record player
-    public XRSocketInteractor socket2; // Socket for the second record player
-    public GameObject fireEffect; // The fire effect GameObject (e.g., particle system)
-    public Text puzzleSolvedText; // Text component to display the solved message
-    public UnityEvent onPuzzleSolved; // Unity Event to trigger additional actions
+    public XRSocketInteractor socket1; // Socket for the first music disk
+    public XRSocketInteractor socket2; // Socket for the second music disk
+    public GameObject fireEffect; // Fire particles on the furnace
+    public Light spotlight; // Spotlight to turn on
+    public TextMeshProUGUI puzzleSolvedText; // TMP for solved message
 
     private bool isRecord1Placed = false;
     private bool isRecord2Placed = false;
 
     private void Start()
     {
-        fireEffect.SetActive(false); // Make sure fire is off at start
-        puzzleSolvedText.gameObject.SetActive(false); // Hide text at start
+        // Ensure all visuals are disabled at the start
+        fireEffect.SetActive(false);
+        spotlight.enabled = false;
+        puzzleSolvedText.gameObject.SetActive(false);
 
-        // Subscribe to the select entered event to know when a record is placed in the socket
+        // Subscribe to the socket events
         socket1.selectEntered.AddListener(OnRecord1Placed);
         socket2.selectEntered.AddListener(OnRecord2Placed);
+
+        // Optionally, handle when objects are removed from the sockets
+        socket1.selectExited.AddListener(OnRecord1Removed);
+        socket2.selectExited.AddListener(OnRecord2Removed);
     }
 
-    // Callback for when the first record is placed
     private void OnRecord1Placed(SelectEnterEventArgs args)
     {
         isRecord1Placed = true;
+        Debug.Log("Record 1 placed");
         CheckPuzzleCompletion();
     }
 
-    // Callback for when the second record is placed
     private void OnRecord2Placed(SelectEnterEventArgs args)
     {
         isRecord2Placed = true;
+        Debug.Log("Record 2 placed");
         CheckPuzzleCompletion();
     }
 
-    // Check if both records are placed to complete the puzzle
+    private void OnRecord1Removed(SelectExitEventArgs args)
+    {
+        isRecord1Placed = false;
+        Debug.Log("Record 1 removed");
+        CheckPuzzleCompletion();
+    }
+
+    private void OnRecord2Removed(SelectExitEventArgs args)
+    {
+        isRecord2Placed = false;
+        Debug.Log("Record 2 removed");
+        CheckPuzzleCompletion();
+    }
+
     private void CheckPuzzleCompletion()
     {
         if (isRecord1Placed && isRecord2Placed)
         {
-            ActivateFire();
-            onPuzzleSolved.Invoke(); // Invoke the Unity Event when puzzle is solved
+            ActivateFireAndLight();
+        }
+        else
+        {
+            DeactivateFireAndLight(); // Turn off the effects if one record is removed
         }
     }
 
-    // Activate the fire and show the puzzle solved message
-    private void ActivateFire()
+    private void ActivateFireAndLight()
     {
-        fireEffect.SetActive(true); // Activate fire
-        puzzleSolvedText.gameObject.SetActive(true); // Show "puzzle solved" text
+        fireEffect.SetActive(true);
+        spotlight.enabled = true;
+        puzzleSolvedText.gameObject.SetActive(true);
+        puzzleSolvedText.text = "You've lit the fire!"; // Show message
+        Debug.Log("Puzzle Solved: Fire activated, spotlight turned on");
+    }
+
+    private void DeactivateFireAndLight()
+    {
+        fireEffect.SetActive(false);
+        spotlight.enabled = false;
+        puzzleSolvedText.gameObject.SetActive(false);
+        Debug.Log("Puzzle Incomplete: Fire deactivated, spotlight turned off");
     }
 }
